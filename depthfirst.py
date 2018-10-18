b579d1348cd6c4bb5a8785749c8c957948e8920c
raphsimon <ra.simon1998@gmail.com>
2018-10-18 21:57:18 +0200

    Ajouté fichier pour le parcours

---
+++ depthfirst.py
@@ -0,0 +1,9 @@
+"""
+Ceci est l'implémentation de l'algorithme de parcours de graphe
+en profondeur pour parcourir notre arbre
+On utilise cet algorithme parce que l'arbre que nous avons est
+un graphe non connexe et les algorithmes pour le parcours d'AB
+ne sont pas adaptés à notre structure de données
+"""
+
+def depthFirst(graph):
