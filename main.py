f9605f027ec954a2212b5121c71e42f109eab6f1
amangrio <32870165+amangrio@users.noreply.github.com>
2018-10-21 17:42:53 +0200

    Ajout en comm de la version avec Netwokx

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from WeightedTree import *
+from new_depthfirst import *
+# from Random_tree import *
+from random import randint
 from treeGeneration import *
 from depthfirst import *
-from random import randint
 
 
 def main():
+
+    # my_tree = tree_Generation(randint(7,15))
+    # max_subtree(my_tree)
+
+
     myTree = treeGeneration()
     print("Weight of root: ", myTree.getWeight())
     print("Number of children root has: ", len(myTree.getAllChildren()))
