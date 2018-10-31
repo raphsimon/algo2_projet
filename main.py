4811df438367ea599a31f5338a162f71a70fa05d
raphsimon <ra.simon1998@gmail.com>
2018-10-31 15:16:46 +0100

    implémenté print_tree

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from depthfirst import *
 from random import randint
 from treeGeneration import *
 from tree_Representation_nx import *
+from print_tree import *
 
 
 def main():
@@ -14,14 +15,20 @@ def main():
     print()
     print("------------Arbre de Base------------")
     #depthFirst(myTree)
-    tree_representation(myTree)
+    
+
+    # tree_representation(myTree)
+    print_tree(myTree)
+
 
     TREE = max_subtree(myTree)
     print()
     print("------------Arbre de Après maximisation------------")
     #depthFirst(TREE)
-    tree_representation(TREE)
+    
 
+    #tree_representation(TREE)
+    print_tree(TREE)
 
 if __name__ == "__main__":
     main()
